41e840c83b3703e67d77deccb173c2c0989b0bb9
JazminHorton <jazmin.horton@edu.sait.ca>
2023-11-24 15:17:30 -0700

    Update page.js
    Started planning out the homepage. Nothing final yet just drawing stuff out. It has a high change to become much simpler than this due to lack of time

--- app/page.js
+++ app/page.js
@@ -1,113 +1,79 @@
-import Image from 'next/image'
+// Description: This page is the main page of the application. It will display a feed of manga and new releases.
+import React from 'react';
+import { Link } from 'next/link';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="homepage-container">
+      {/* Hero Section */}
+      <div className="hero-section">
+        <h1>Welcome to ChapterPulse</h1>
+        <p>Your Community for Manga Enthusiasts</p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Trending Manga Reviews */}
+      <div className="trending-reviews-section">
+        <h2>Trending Manga Reviews</h2>
+        {/* Display a carousel or grid of trending manga */}
+        {/* Each review card can include cover image, title, and snippets */}
+        {/* Read More link to the detailed page */}
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* New Releases */}
+      <div className="new-releases-section">
+        <h2>New Releases</h2>
+        {/* Display recently added manga series or chapters */}
+        {/* Include cover images, titles, and publication dates */}
+        {/* Quick links to explore and read the latest releases */}
       </div>
-    </main>
-  )
-}
+
+      {/* Featured Discussions */}
+      <div className="featured-discussions-section">
+        <h2>Featured Discussions</h2>
+        {/* Showcase popular or ongoing discussions with user avatars, titles, and snippets */}
+        {/* Link to join the discussions */}
+      </div>
+
+      {/* Discover More */}
+      <div className="discover-more-section">
+        <h2>Discover More</h2>
+        {/* Sections for genres, popular tags, and top contributors */}
+        {/* Each with clickable icons, links, or user avatars */}
+      </div>
+
+      {/* Call-to-Action Buttons */}
+      <div className="cta-buttons">
+        <Link href="/register">
+          <a className="join-community-button">Join the Community</a>
+        </Link>
+        <Link href="/reviews">
+          <a className="explore-reviews-button">Explore Reviews</a>
+        </Link>
+        <Link href="/create-discussion">
+          <a className="start-discussion-button">Start a Discussion</a>
+        </Link>
+      </div>
+
+      {/* Footer */}
+      <div className="footer">
+        {/* Essential links and social media icons */}
+        <p>About Us | Privacy Policy | Terms of Service</p>
+        {/* Social media icons or links */}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
+
+
+//My main page is going to be a feed of reviews, discussions, and new releases.
+//I want the main function of the project to be that users can make an account and view manga's general information.
+//they can add the manga to their list of manga they are reading, and by doing so get notifications when a new chapter is released.
+//They cannot read the manga on the site
+//(MAYBE) They can write reviews and rate the manga
+//(MAYBE) They can discuss the manga with other users
+//They can search for manga by title, author, genre, and tags
+//They can view the most popular manga
+//I'll have to request the API for the manga information from MyAnimeList, and then display it on the page.
+//I'll have to ask for assistance on how to do this, as I'm not sure how to do it completely on my own.
