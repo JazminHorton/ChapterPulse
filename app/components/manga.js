e3c97dd3b5d9ec0d97db2ca4d6047b244373231d
JazminHorton <jazmin.horton@edu.sait.ca>
2023-12-01 08:16:50 -0700

    Idea at fetching api data

---
+++ app/components/manga.js
@@ -0,0 +1,35 @@
+//IDEA: This is the manga component that will be used to display the manga information
+//I have no idea how to do this but I'm gonna try my best
+//I might need to ask Aaron for help with this one. 
+//I might need to make multiple components for this one. Like one for the manga information and one for the manga list.
+//One for the manga information that will be used in the search page and one for the manga list that will be used in the list page.
+//This is a bit more complicated than I thought, plus it's more work than the assignment.
+//I'm not sure if I'll be able to finish this in time. If not, I'll just ask Aaron if I can just hard code some manga in there as examples.
+
+
+"use client";
+import React, { useState, useEffect } from 'react';
+
+async function getManga() {
+  try {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/manga`
+    );
+    const data = await response.json();
+
+    if (data && data.length > 0) {
+      const mangaDetails = data.meals.map((meal) =>
+        fetch(
+          `https://api.jikan.moe/v4/manga/'`
+        ).then((res) => res.json())
+      );
+
+      const mealDetails = await Promise.all(mangaDetails);
+      return mealDetails.map((detail) => detail.meals[0]);
+    }
+    return [];
+  } catch (error) {
+    console.error('There was a problem with the fetch operation: ', error.message);
+    throw error;
+  }
+}
--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24' style={containerStyle}>
       <nav className="bg-black text-white border border-white p-4 rounded-lg text-center">
+        <img src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjExNjAtMDM2LnBuZw.png" alt="ChapterPulse Logo" className="w-24 h-24 mr-2" />
         <h1 className="text-6xl text-white mb-6">ChapterPulse</h1>
         <div className="group hover:text-green-500">
         </div>
@@ -24,22 +25,27 @@ export default function Home() {
               </li>
               <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
                 <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+                  <Link href="/">Log out</Link>
+                </button>
+              </li>
+              <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
                   <Link href="/">Manga</Link>
                 </button>
               </li>
               <li className="mr-5 transition duration-300 ease-in-out transform hover:scale-105">
                 <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
-                  <Link href="/">List</Link>
+                  <Link href="/">Your List</Link>
                 </button>
               </li>
               <li className = "mr-5 transition duration-300 ease-in-out transform hover:scale-105">
                 <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
-                  <Link href="/">Search</Link>
+                  <Link href="/">Search for Manga</Link>
                 </button>
               </li>
               <li className = "mr-5 transition duration-300 ease-in-out transform hover:scale-105">
                 <button className="inline-block p-2 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
-                  <Link href="/">Profile</Link>
+                  <Link href="/">Your Profile</Link>
                 </button>
               </li>
               </ul>
